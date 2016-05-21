97f98024c90a3acefdcf3e3ffa0dd00e36fe6b5d
LeverNucky <wqricardo@gmail.com>
2016-05-22 02:30:11 +0800

    2016/5/22
    add stopwords
    ranking hot words better

--- jieba_test.py
+++ jieba_test.py
@@ -3,17 +3,27 @@ import jieba.analyse
 import json
 from collections import OrderedDict
 
+
 path="H:/SJTU/data/baidu/data/dongfangzhixing/dongfangzhixing_query_sample"
 text_path="H:/SJTU/data/baidu/data/dongfangzhixing/text_data.txt"
 with open(path,'rb') as f:
     data=f.readlines()
 
 d=OrderedDict()
+
+#jieba.analyse.set_stop_words("dict/stopwords.txt")
+
+stopwords=[line.strip() for line in open('dict/stopwords.txt','rb').readlines()]
+
+for i in range(len(stopwords)):
+    stopwords[i]=stopwords[i].decode('utf8')
+    
+jieba.load_userdict("dict/event.txt")
 #text_data=open(text_path,'w')
 #print type(data),data[0]
 for each in data:
     contents=json.loads(each)
-    result=jieba.lcut_for_search(contents['text'])
+    result=jieba.lcut(contents['text'])
     for word in result:
         if word:
             if word in d:
@@ -22,9 +32,11 @@ for each in data:
                 d[word]=1
 #bar=OrderedDict(sorted(d.items(), key=lambda x: x[1]))
 bar=sorted(d.items(), key=lambda x: x[1],reverse=True)
+print(type(bar))
 top=(bar[:50])
 for i in top:
-    print i[0],i[1]
+    if i[0] not in [u' ',u',',u'"',u'.',u'?']+stopwords:
+        print i[0],i[1]
 
 #print json.dumps(bar[:10],indent=4)
 '''                
